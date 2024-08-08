ed909d3c6089508d48b73c251bcd325c1ed97f9f
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-08-08 22:35:49 +0530

    implemented job screen

--- app/src/main/java/com/example/amansingh/presentation/JobScreen.kt
+++ app/src/main/java/com/example/amansingh/presentation/JobScreen.kt
@@ -1,11 +1,45 @@
 package com.example.amansingh.presentation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.amansingh.ui.theme.Blur
+import dev.chrisbanes.haze.HazeState
+import dev.chrisbanes.haze.HazeStyle
+import dev.chrisbanes.haze.haze
 
 
 @Composable
-fun JobScreen(modifier: Modifier= Modifier)
+fun JobScreen(modifier: Modifier= Modifier,
+              hazeState: HazeState, paddingValues: PaddingValues)
 {
 
+
+
+
+
+    Box(modifier = modifier
+        .background(MaterialTheme.colorScheme.background)
+    )
+    {
+
+        JobList(
+            modifier = Modifier
+                .fillMaxSize()
+                .haze(
+                    state = hazeState,
+                    style = HazeStyle(
+                        blurRadius = 13.dp, tint = Blur
+                    )
+                ),
+            paddingValues = paddingValues
+        )
+    }
 }
+
+
